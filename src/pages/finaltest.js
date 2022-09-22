d008ccfd8d510c89b6d93dc0e3001d3f2030bd55
Neerav Garg <neeravgarg366@gmail.com>
2022-09-23 00:37:36 +0530

    second

--- src/pages/finaltest.js
+++ src/pages/finaltest.js
@@ -1,43 +1,46 @@
 import React, { useEffect } from "react";
 import useMediaQuery from "../components/hooks/useMediaQuery";
+import IndicatorCards from "../components/indicatorCards";
 import { requireAuth } from "../components/utils/requireAuth";
 import { formAuth } from "../components/utils/formAuth";
 import SidebarBehave from "../components/utils/sidebarBehave";
-import IndicatorCards from "../components/indicatorCards";
 
-export default function Finaltest() {
+export default function PracticeTest() {
   const isLaptop = useMediaQuery("(min-width: 1024px)");
   const isTablet = useMediaQuery("(min-width: 768px )");
 
   useEffect(() => {
     formAuth();
   }, []);
-
   return (
     <>
+      {/* <FormAuth /> */}
       <SidebarBehave>
         {/* main page */}
+
         <div
           className={`flex flex-col  w-screen  gap-12 pb-10 px-1 items-center
         ${isTablet && "gap-10 "} ${isLaptop && "gap-14  min-h-screen"} `}
         >
           <div
-            className={`flex flex-col h-52 rounded-3xl bg-skin-base dark:bg-gradient-to-r from-[#323232] to-[#292929] dark:theme-dark shadow-md py-7 items-center justify-end text-center gap-4 w-full  ${isLaptop &&
-              "w-9/12"} 
+            className={`flex flex-col h-52   rounded-3xl bg-skin-base dark:bg-gradient-to-r from-[#323232] to-[#292929] dark:theme-dark shadow-md py-7 items-center justify-end text-center  gap-4  w-full  ${
+              isLaptop && "w-9/12"
+            } 
             ${isTablet && "w-11/12"} 
            `}
           >
             <span
-              className={`text-3xl text-skin-base bg-skin-muted border-2 border-skin-base px-3 py-3 w-9/12 rounded-xl shadow-sm dark:bg-zinc-700 font-bold capitalize `}
+              className={` text-3xl text-skin-base bg-skin-muted border-2 border-skin-base px-3 py-3 w-9/12 rounded-xl shadow-sm  dark:theme-dark font-bold capitalize `}
             >
-              FINAL-TEST
+              FINAL TEST
             </span>
 
             <span
-              className={`${isTablet &&
-                "text-xl"} text-lg text-skin-muted dark:theme-dark italic`}
+              className={`${
+                isTablet && "text-xl"
+              } text-lg text-skin-muted dark:theme-dark italic `}
             >
-              Let's start the Unit test
+              Let's start the Final Test
             </span>
           </div>
           <div
@@ -47,7 +50,7 @@ export default function Finaltest() {
           >
             {/* selection card */}
             <div
-              className={` flex gap-8 w-full 
+              className={` grid grid-rows-2  gap-8 w-full 
             ${isTablet && "w-10/12"} ${isLaptop && "w-8/12"}`}
             >
               {/* card */}
